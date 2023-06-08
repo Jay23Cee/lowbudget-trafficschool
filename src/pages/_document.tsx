421fee8d89b3975d2f94223b716ab2b746484c8e
JAYCEE <Jayceexxiii@gmail.com>
2023-06-07 19:41:04 -0700

    update analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,24 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
       <Html lang="en">
         <Head>
           {/* Google Analytics tracking code */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-0H26C5QZWE"></script>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q95W0CG8M8"></script>
           <script
             dangerouslySetInnerHTML={{
               __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', 'G-0H26C5QZWE');
+                gtag('config', 'G-Q95W0CG8M8');
               `,
             }}
           />
@@ -26,3 +31,5 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+export default MyDocument;
